1b00cb77a7d0df5ba6ddc76e4835b7e7a08022e4
Raul Exposito Campos <raulrecrec@gmail.com>
2024-11-04 17:49:39 +0100

    deleted all commentaries

--- tests/main_tests.c
+++ tests/main_tests.c
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 19:11:02 by rexposit          #+#    #+#             */
-/*   Updated: 2024/10/29 19:16:34 by rexposit         ###   ########.fr       */
+/*   Updated: 2024/11/04 17:48:09 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "get_next_line.h"
 
 void run_test(const char *filename) {
-	int fd = open(filename, O_RDONLY);  // Abre el archivo en modo lectura
+	int fd = open(filename, O_RDONLY);
 	if (fd == -1) {
 		perror("Error al abrir el archivo");
 		return;
@@ -24,18 +24,15 @@ void run_test(const char *filename) {
 	char *line;
 	int line_number = 1;
 
-	// Leer el archivo línea por línea con get_next_line
 	while ((line = get_next_line(fd)) != NULL) {
-		printf("Línea %d: %s\n", line_number++, line);  // Imprime cada línea devuelta por get_next_line
-		free(line);  // Libera la memoria de la línea después de usarla
+		printf("Línea %d: %s\n", line_number++, line);
+		free(line);
 	}
-
-	close(fd);  // Cierra el archivo después de terminar de leer
+	close(fd);
 	printf("=== Fin de lectura de: %s ===\n\n", filename);
 }
 
 int main() {
-	// Lista de archivos de prueba
 	const char *test_files[] = {
 		"archivo_vacio.txt",
 		"una_sola_linea.txt",
@@ -46,16 +43,13 @@ int main() {
 		"sin_saltos.txt",
 		"lineas_vacias.txt",
 		"caracteres_especiales.txt",
-		"archivo_inexistente.txt"  // Archivo para probar el manejo de errores
+		"archivo_inexistente.txt"
 	};
 
 	int num_tests = sizeof(test_files) / sizeof(test_files[0]);
-
-	// Ejecuta cada prueba
 	for (int i = 0; i < num_tests; i++) {
 		printf("Iniciando prueba para archivo: %s\n", test_files[i]);
 		run_test(test_files[i]);
 	}
-
 	return 0;
 }
