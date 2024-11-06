932152c4ff687c93e7ea990ccc1c12609a56dd5a
Raul Exposito Campos <raulrecrec@gmail.com>
2024-11-06 13:49:22 +0100

    now buffer size is not redefined when is used incorrectly

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 18:09:36 by rexposit          #+#    #+#             */
-/*   Updated: 2024/11/04 14:33:51 by rexposit         ###   ########.fr       */
+/*   Updated: 2024/11/06 12:24:47 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,31 +17,7 @@
 # include <stdlib.h>
 # include <stdio.h>
 
-# ifndef FD_MAX
-#  define FD_MAX FOPEN_MAX
-# endif
-
-# if FD_MAX > FOPEN_MAX
-#  undef FD_MAX
-#  define FD_MAX FOPEN_MAX
-# endif
-
-# if FD_MAX < 1
-#  undef FD_MAX
-#  define FD_MAX FOPEN_MAX
-# endif
-
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE BUFSIZ
-# endif
-
-# if BUFFER_SIZE > 1000000
-#  undef BUFFER_SIZE
-#  define BUFFER_SIZE BUFSIZ
-# endif
-
-# if BUFFER_SIZE < 1
-#  undef BUFFER_SIZE
 #  define BUFFER_SIZE BUFSIZ
 # endif
 
